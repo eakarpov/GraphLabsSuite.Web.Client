0516b9c8388c0496a041240462a9751c3a343e11
eakarpov <eakarpov@yandex.ru>
2018-11-01 00:05:00 +0300

    welcome title

---
+++ src/app/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import {Component} from "react";
+import * as React from "react";
+
+export default class Home extends Component {
+  public render() {
+    return (
+      <div>
+        <h3>Добро пожаловать в GraphLabs!</h3>
+      </div>
+    )
+  }
+}
--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import {Route, Switch} from "react-router";
 import Modules from "../pages/Modules";
 import Module from "../pages/Module";
+import Home from "../pages/Home";
 
 const Routes = () => (
     <div style={{ minHeight: 'calc(100vh - 56px - 23px)', height: 'calc(100vh - 56px - 23px)' }}>
         <Switch>
-            <Route path="/" exact component={/* tslint:disable-line */() => null} />
+            <Route path="/" exact component={Home} />
             <Route path="/modules" component={Modules}/>
             <Route path="/module/:moduleId" component={Module}/>
         </Switch>
