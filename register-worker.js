c9690cd4805edfa441f8e1636714dcf3c14d7821
eakarpov <eakarpov@yandex.ru>
2018-12-20 08:49:08 +0300

    js code commented

--- register-worker.js
+++ register-worker.js
@@ -1,14 +1,14 @@
-const registerServiceWorker = function(){
-    navigator.serviceWorker.register('service-worker.js', { scope: './' })
-        .then(() => navigator.serviceWorker.ready)
-        .then(function () {
-            console.log('service worker registered');
-        })
-        .catch(function (error) {
-            console.error('error when registering service worker', error, arguments)
-        });
-};
-
-module.exports = {
-    registerServiceWorker,
-};
+// const registerServiceWorker = function(){
+//     navigator.serviceWorker.register('service-worker.js', { scope: './' })
+//         .then(() => navigator.serviceWorker.ready)
+//         .then(function () {
+//             console.log('service worker registered');
+//         })
+//         .catch(function (error) {
+//             console.error('error when registering service worker', error, arguments)
+//         });
+// };
+//
+// module.exports = {
+//     registerServiceWorker,
+// };
