ff7bcbd605c532a887ea12cbdac96775c35158db
eakarpov <eakarpov@yandex.ru>
2019-04-15 19:52:14 +0300

    filter query

--- src/api/index.ts
+++ src/api/index.ts
@@ -26,6 +26,16 @@ class Api {
         return Connector.upload(`odata/taskModules(1)/upload`, archive, 'application/zip');
     }
 
+    public getResults(filter?: any) {
+        let url = '';
+        if (filter) {
+            Object.keys(filter).forEach((key: string) => {
+               url += `$filter=substringof('${key}', ${filter[key]}) eq true`;
+            });
+        }
+        return Connector.get('odata/taskVariantLogs' + url);
+    }
+
     private url = (ID: number, file: string) => `odata/taskModules(${ID})/download(path='${file}')`;
 }
 
